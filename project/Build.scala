2a1c95e742f2c7bf138cacab78c564fc6ad2e157
Nicholas Marshall <nmarshall23@gmail.com>
2013-12-31 11:45:59 -0500

    Switched to a Full SBT Configuration.

---
+++ project/Build.scala
@@ -0,0 +1,45 @@
+import sbt._
+import Keys._
+import com.typesafe.sbt.SbtStartScript
+
+object BuildSettings {
+  val buildOrganization = "com.github.nmarshall23"
+  val buildVersion      = "0.1.1-SNAPSHOT"
+  val buildScalaVersion = "2.10.3"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization := buildOrganization,
+    version      := buildVersion,
+    scalaVersion := buildScalaVersion
+  )
+}
+
+object Dependencies {
+  val liftVersion = "2.5.1"
+
+  val liftWeb      = "net.liftweb" %% "lift-webkit" % liftVersion % "compile"
+  val jettyWebApp  = "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "container,test"
+  val javaxServlet = "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container,compile" artifacts Artifact("javax.servlet", "jar", "jar")
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
+
+}
+
+object Lift2Build extends Build {
+  import Dependencies._
+  import BuildSettings._
+
+  val commonDeps = Seq (
+      liftWeb,
+      jettyWebApp,
+      javaxServlet
+    )
+
+  lazy val liftapp = Project("liftweb",
+                              base = file("."),
+                              settings = buildSettings ++ 
+                              Seq (libraryDependencies ++= commonDeps) ++ 
+                              com.earldouglas.xsbtwebplugin.WebPlugin.webSettings ++
+                              SbtStartScript.startScriptForWarSettings
+                              ) 
+
+}
