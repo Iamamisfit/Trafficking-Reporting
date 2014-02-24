ca4764c6e87ac20db627f00ae874001b9974d48b
Nicholas Marshall <nmarshall23@gmail.com>
2014-02-24 02:04:47 -0500

    working on submitter contact ino form.

---
+++ src/main/scala/code/snippet/userSurvey.scala
@@ -0,0 +1,34 @@
+package code 
+package snippet 
+
+import scala.xml.{NodeSeq, Text}
+import net.liftweb.util._
+import net.liftweb.common._
+import java.util.Date
+import code.lib._
+import net.liftweb.http._
+import net.liftweb.common._
+import net.liftweb.util.Helpers._
+
+class UserSurvey extends StatefulSnippet {
+
+  // state unique to this instance of the stateful snippet
+  private var name = ""
+  private var phone_number = ""
+
+  // StatefulSnippet requires an explicit dispatch
+  //   // to the method.
+  def dispatch = {case "render" => render}
+
+  // associate behavior with each HTML element
+  def render = 
+    "name=name" #> SHtml.text(name, name = _, "id" -> "the_name") &
+    "name=phone_number" #> SHtml.text(phone_number, phone_number = _) &
+    "type=submit" #> SHtml.onSubmitUnit(process)
+
+
+  // process the form
+  // Fixme save data to database
+  private def process() = S.redirectTo("/")
+
+}
