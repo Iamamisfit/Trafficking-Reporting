a2d223020178a31ffc3c471e74f98ce512feaddd
Nicholas Marshall <nmarshall23@gmail.com>
2014-03-01 15:43:41 -0500

    Opps forgot to add models to db schemifier.

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -39,7 +39,8 @@ class Boot {
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User)
+    Schemifier.schemify(true, Schemifier.infoF _, ReporterInformation)
+    Schemifier.schemify(true, Schemifier.infoF _, TraffickingReport)
 
     // where to search snippet
     LiftRules.addToPackages("code")
